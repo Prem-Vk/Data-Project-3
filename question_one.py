6d19b5b81a3e8f945243af1e39bf7107fde9eb07
Prem-Vk <premvk976@gmail.com>
2021-11-11 12:41:43 +0530

    Question one solved

--- question_one.py
+++ question_one.py
@@ -1,8 +1,21 @@
 from matplotlib import pyplot as plt
 import csv
+import seaborn as sns
 
-with open("population-estimates_csv", "r") as raw_data:
+years = []
+population = []
+
+with open("SAARC.csv", "r") as raw_data:
     data_reader = csv.reader(raw_data)
 
     for line in data_reader:
-        print(line)
+        if line[0] == "India":
+            years.append(int(line[2]))
+            population.append(int(float(line[3])))
+
+plt.title("India Population")
+sns.barplot(x=years, y=population)
+plt.xticks(rotation=90)
+plt.xlabel("Years")
+plt.ylabel("Population")
+plt.show()
